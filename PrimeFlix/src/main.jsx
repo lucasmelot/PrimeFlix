3bfdf2497e0a9981a56fcee2cb8493e2055e2ce8
Lucas Melo <lucasandradedemelo377@gmail.com>
2026-05-22 12:58:23 -0300

    Criando a estrutura do projeto e adicionando as Routes para navegação de páginas

--- PrimeFlix/src/main.jsx
+++ PrimeFlix/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './assets/components/App.jsx'
+import './style.css'
 
 createRoot(document.getElementById('root')).render(
   <App />
---
+++ PrimeFlix/src/routes.jsx
@@ -0,0 +1,19 @@
+// Instalar react-router-dom 
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Filme from './pages/Filme'
+
+function RoutesApp(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <Home/> } /> 
+                <Route path="/filme/:id" element={ <Filme/> } /> 
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RoutesApp;
